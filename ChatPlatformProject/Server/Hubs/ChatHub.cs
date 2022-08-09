3f058a10ee3151de8f9d3a7679d6e573703c0d41
Flash1714 <shaurya1714@gmail.com>
2022-08-09 18:19:42 +0530

    made the C# part of the client side code

--- ChatPlatformProject/Server/Hubs/ChatHub.cs
+++ ChatPlatformProject/Server/Hubs/ChatHub.cs
@@ -9,13 +9,13 @@ namespace ChatPlatformProject.Server.Hubs
         public override async Task OnConnectedAsync()
         {
             // Uses the function below to acknowledge new person joining in.
-            await SendMessage("", "New User Connected!"); 
+            await AddMessageToChat("", "New User Connected!"); 
             await base.OnConnectedAsync(); //call base ????
         }
         
 
         //For sending messages  (Made first)
-        private async Task SendMessage(string user, string message)
+        private async Task AddMessageToChat(string user, string message)
         {
             // Sends the Hub update to all clients (users). "RecieveMessage" is an identifier.
             await Clients.All.SendAsync("RecieveMessage", user, message);
