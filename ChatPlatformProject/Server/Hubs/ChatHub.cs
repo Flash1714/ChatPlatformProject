86c27638405edb9d397773ec34f6f0be461ca294
Flash1714 <shaurya1714@gmail.com>
2022-08-10 12:57:28 +0530

    added proper greets to the chat and updated counter to cookie clicker.

--- ChatPlatformProject/Server/Hubs/ChatHub.cs
+++ ChatPlatformProject/Server/Hubs/ChatHub.cs
@@ -4,15 +4,34 @@ namespace ChatPlatformProject.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        //making a hashmap for storing username and connection id 
+
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        
+        
         //When user connects (made second becauses it uses the first function)
 
         public override async Task OnConnectedAsync()
         {
             // Uses the function below to acknowledge new person joining in.
-            await AddMessageToChat("", "New User Connected!"); 
+            
+            //Requesting username 
+            string username = Context.GetHttpContext().Request.Query["username"];
+            // Adding username to hashmap along with connection id
+            Users.Add(Context.ConnectionId,username);  
+
+
+            await AddMessageToChat(String.Empty, $"{username} has joined the chat!"); 
             await base.OnConnectedAsync(); //call base ????
         }
-        
+
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(String.Empty, $"{username} left :(");
+            
+        }
 
         //For sending messages  (Made first)
         public async Task AddMessageToChat(string user, string message)
@@ -22,5 +41,7 @@ namespace ChatPlatformProject.Server.Hubs
         }
 
         // Satarting to make the chat hub program : after this we add service and response compression in program.cs
+
+        //We can use connection id to identify a user hence make usernames possible
     }
 }
